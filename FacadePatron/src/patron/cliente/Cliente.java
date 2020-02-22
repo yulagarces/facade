f279f2d1548878d5a7e71721cb9f391a859dd5fc
Edgar Nova <ragnarok540@gmail.com>
2020-02-22 13:08:56 -0500

    Prueba Fork

--- FacadePatron/src/patron/cliente/Cliente.java
+++ FacadePatron/src/patron/cliente/Cliente.java
@@ -6,11 +6,11 @@ public class Cliente {
 
     public static void main(String[] args) {
 
-        System.out.println("--------------- ENCENDER ---------------");
+        System.out.println("--------------- ENCENDER ---------------\n");
         Fachada fachada = new Fachada();
         DispositivoMovil dispositivo = fachada.encender();
         System.out.println();
-        System.out.println("--------------- APAGAR ---------------");
+        System.out.println("--------------- APAGAR ---------------\n");
         fachada.apagar(dispositivo);
     }
 }
